dfa492668e57a86eb0de2e955b96820d6cfb1734
D.Mentia <dimencia@gmail.com>
2022-04-25 20:55:23 -0400

    Add project files.

---
+++ BardMidiApi/Models/MidiContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BardMidiApi.Models
+{
+    public class MidiContext : DbContext
+    {
+        public MidiContext(DbContextOptions<MidiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MidiItem> MidiItems { get; set; } = null!;
+    }
+}
---
+++ BardMidiApi/Models/MidiItem.cs
@@ -0,0 +1,12 @@
+﻿namespace BardMidiApi.Models
+{
+    public class MidiItem
+    {
+        public long Id { get; set; }
+        public long Hash { get; set; } // Technically could collide, so we won't use it as our key
+        public string? Name { get; set; }
+        public string? Author { get; set; }
+        public int? Score { get; set; }
+        public string? DownloadUrl { get; set; }
+    }
+}
